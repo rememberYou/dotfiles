d650c35101a35cc0bfa1208f22d56c316c3c8d8c
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-03-26 22:39:15 +0200

    Fix redshift script with i3

---
+++ bin/bin/whereami.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+# The main purpose of this script is to allow you to dynamically adjust
+# the color temperature of your screen according to your location using
+# redshift for your windows manager.
+#
+# Another cool feature of this script is that it allows you to easily
+# publish the configuration of your windows manager to GitHub without
+# revealing your location.
+
+import json
+import urllib.request
+
+url = 'http://ipinfo.io/json'
+
+def is_internet():
+    """Tells if there is Internet access.
+
+    Returns:
+        bool: True if there is Internet access. Otherwise, False.
+
+    """
+    try:
+        urllib.request.urlopen(url, timeout=1)
+        return True
+    except urllib.error.URLError:
+        return False
+
+def get_location():
+    """Gets your location according to the WiFi signals.
+
+    Returns:
+        str: The location in the following format (lat,long).
+
+    """
+    try:
+        return json.load(urllib.request.urlopen(url))
+    except urllib.error.URLError:
+        return False
+
+def get_default(path):
+    """Gets the default location.
+
+    Returns:
+        str: The default location stored in a file.
+
+    """
+    with open(path, 'r') as f:
+        return f.read().replace('\n', '')
+
+if __name__ == '__main__':
+    if is_internet():
+        print(get_location()['loc'].replace(',', ':'))
+    else:
+        print(get_default("/home/someone/Dropbox/shared/localization"))
