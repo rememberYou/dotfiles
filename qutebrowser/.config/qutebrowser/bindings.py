02912d643e5e67efdd80e2ea9a05548ea4e362c3
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-11-26 07:17:01 +0100

    Add more bindings for qutebrowser

--- qutebrowser/.config/qutebrowser/bindings.py
+++ qutebrowser/.config/qutebrowser/bindings.py
@@ -1,15 +1,19 @@
 config.bind('<Ctrl-Shift-tab>', 'tab-prev')
 config.bind('<Ctrl-Tab>', 'tab-next')
+config.bind('<Ctrl-h>', 'history')
+config.bind('<Ctrl-r>', 'config-source')
+
 config.bind('<Shift-h>', 'run-with-count 20 scroll left')
 config.bind('<Shift-j>', 'run-with-count 20 scroll down')
 config.bind('<Shift-k>', 'run-with-count 20 scroll up')
 config.bind('<Shift-l>', 'run-with-count 20 scroll right')
+
+config.bind('<', 'back')
+config.bind('>', 'forward')
 config.bind('h', 'run-with-count 2 scroll left')
 config.bind('j', 'run-with-count 2 scroll down')
 config.bind('k', 'run-with-count 2 scroll up')
 config.bind('l', 'run-with-count 2 scroll right')
-config.bind('<', 'back')
-config.bind('>', 'forward')
 
 config.bind(',d', 'spawn youtube-dl -o ~/Videos/%(title)s.%(ext)s {url}')
 config.bind(',p', 'spawn --userscript qutepass.py --password-store ~/.local/share/password-store --dmenu-invocation dmenu')
