1757451f92075cb0f082f5e8307354b15c130646
Terencio Agozzino <terencio.agozzino@gmail.com>
2019-03-18 09:04:30 +0100

    Delete comments for launch.sh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
 polybar top
-
-
-echo "Bars launched..."
