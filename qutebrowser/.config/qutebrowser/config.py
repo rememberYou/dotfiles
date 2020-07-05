0de1cf9eeea49e73807e54a19f37dfc212769e02
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-05 07:41:41 +0200

    Fix typo

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,36 +2,36 @@ import os.path
 
 c.qt.force_platform = 'xcb'
 c.completion.web_history.max_items = 10000
-c.content.default_encoding = 'utf-8'
+c.content.default_encoding = "utf-8"
 c.content.javascript.enabled = False
 c.content.pdfjs = True
-c.downloads.location.directory = os.path.expanduser('~/Downloads')
+c.downloads.location.directory = os.path.expanduser("~/Downloads")
 c.downloads.location.prompt = False
 c.downloads.remove_finished = 10000
-c.editor.command = ['em', '{}']
+c.editor.command = ["em", "{}"]
 c.hints.uppercase = True
 c.keyhint.delay = 100
 c.scrolling.smooth = True
 c.tabs.background = True
-c.tabs.title.format = '{perc}{current_title}'
+c.tabs.title.format = "{perc}{current_title}"
 c.url.default_page = "https://news.ycombinator.com/"
 
 # Load binding setting.
-config.source('bindings.py')
+config.source("bindings.py")
 
 # Load fonts setting.
-config.source('fonts.py')
+config.source("fonts.py")
 
 # Load search engines setting.
-config.source('search_engines.py')
+config.source("search_engines.py")
 
 # Load nord theme setting.
-config.source('themes/nord.py')
+config.source("themes/nord.py")
 
 js_whitelist = [
     "*://*.youtube.com/*",
     "*://127.0.0.1/*",
-    "*://darksky.net/*"
+    "*://darksky.net/*",
     "*://deepl.com/*",
     "*://duckduckgo.com/*",
     "*://github.com/*",
@@ -41,7 +41,9 @@ js_whitelist = [
     "*://translate.google.com/*",
 ]
 
-private_whitelist = os.path.expanduser("~/.config/qutebrowser/private-whitelist")
+private_whitelist = os.path.expanduser(
+    "~/.config/qutebrowser/private-whitelist"
+)
 if os.path.exists(private_whitelist):
     with open(private_whitelist) as f:
         js_whitelist += filter(lambda l: bool(l), f.read().split("\n"))
