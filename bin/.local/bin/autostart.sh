1f1ef4c0c8b9d5fbcf4bf0808a170a338b5ecc98
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-05 17:40:15 +0200

    Remove xorg things in autostart.sh

--- bin/.local/bin/autostart.sh
+++ bin/.local/bin/autostart.sh
@@ -4,17 +4,7 @@
 
 have() { type "$1" > /dev/null 2>&1; }
 
-if have picom; then
-    if [ -f "$XDG_CONFIG_HOME/picom/picom.conf" ]; then
-        picom --config "$XDG_CONFIG_HOME/picom/picom.conf" &
-    else
-        picom &
-    fi
-fi
-
-if pgrep i3 && have dunst; then
-    dunst &
-elif pgrep sway && have mako; then
+if  pgrep sway && have mako; then
 	mako --background-color "#2e3440" --border-color "#5e81ac" --font "Source Code Pro Medium 12" --padding 20 --default-timeout 10000 &
 fi
 
@@ -22,20 +12,8 @@ if have emacs; then
     emacs --daemon &
 fi
 
-if have feh; then
-    feh --no-fehbg --bg-center `wallpaper.py` &
-fi
-
-if have nm-applet; then
-    nm-applet &
-fi
-
 if have mpd; then
     [ ! -s ~/.config/mpd/pid ] && mpd &
-fi
-
-if have polybar; then
-    [ -f "$XDG_CONFIG_HOME/polybar/launch.sh" ] && . "$XDG_CONFIG_HOME/polybar/launch.sh" &
 fi
 
 if have razercfg; then
@@ -48,10 +26,6 @@ fi
 
 if have syncthing; then
     syncthing -no-browser &
-fi
-
-if have unclutter; then
-    unclutter --ignore-scrolling --jitter 2 &
 fi
 
 if have xbindkeys; then
